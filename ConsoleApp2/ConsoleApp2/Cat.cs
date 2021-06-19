abe548753853297a0fcb186a9ca89b23c98c895e
Kamran999A <85412897+Kamran999A@users.noreply.github.com>
2021-06-20 00:43:23 +0400

    com

---
+++ ConsoleApp2/ConsoleApp2/Cat.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Threading;
+using System.Text;
+namespace Cat_House
+{
+    public class Cat
+    {
+        public string NickName { get; set; } = String.Empty;
+        private int _age;
+        private int _price;
+        private string _gender;
+        private int _mealQuantity;
+        public int Energy { get; private set; } = 100;
+        private int _mealQuantityOfCatToday = default;
+        public Cat(in string nickName, in int age, in int mealQuantity, in int price, in string gender)
+        {
+            NickName = nickName;
+            Age = age;
+            MealQuantity = mealQuantity;
+            Price = price;
+            Gender = gender;
+            _mealQuantityOfCatToday = (MealQuantity * 10) / 100;
+        }
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value <= 0)
+                    throw new InvalidOperationException("Age must be more than -1.");
+                _age = value;
+            }
+        }
+        public int MealQuantity
+        {
+            get
+            {
+                return _mealQuantity;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new InvalidOperationException("Meal Quantity must be more than 0.");
+                _mealQuantity = value;
+            }
+        }
+        public int Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new InvalidOperationException("Price must be more than 0.");
+                _price = value;
+            }
+        }
+        public string Gender
+        {
+            get
+            {
+                return _gender;
+            }
+            set
+            {
+                if (value.Trim().ToLower() != "male" && value.Trim().ToLower() != "female")
+                    throw new InvalidOperationException("Gender must be \"Male\" or \"Female\"");
+                _gender = value;
+            }
+        }
+        public void Eat()
+        {
+            if (Energy == 0)
+            {
+                Console.WriteLine("Energy is 0 go to sleep. Cat can't eat something with 0 Energy.");
+                return;
+            }
+                IncrementPrice(ref _price);
+                IncrementMealQuantity(ref _mealQuantityOfCatToday);
+            const int eatingEnergy = 20;
+            if (Energy + eatingEnergy <= 100)
+                Energy += eatingEnergy;
+        }
+        public void Play()
+        {
+            const int playEnergy = 10;
+            if (Energy - playEnergy >= 0)
+            { Energy -= playEnergy; 
+            Show(); 
+                Thread.Sleep(500);
+            }
+            else
+            {
+                Console.WriteLine("Energy is 0 go to sleep. Cat can't play with 0 Energy.");
+                Show();
+                Sleep();
+                Thread.Sleep(500);
+                Console.Clear();
+            }
+        }
+        public void Sleep()
+        {
+            const int sleepEnergy = 20;
+            if (Energy + sleepEnergy <= 100)
+                Console.WriteLine($"{Energy += sleepEnergy}");
+                IncrementPrice(ref _price);
+            Show();
+            Thread.Sleep(800);
+        }
+        static public void IncrementPrice(ref int price) => price += (price * 10) / 100;
+        static public void IncrementMealQuantity(ref int mealQuantity) => mealQuantity += (mealQuantity * 85) / 100;
+        public void Show()
+        {
+            if(Energy == 0)
+            {
+                Sleep();
+                Console.Clear();
+            }
+            string trimVersionOfGender = Gender.Trim();
+            StringBuilder gender = new StringBuilder();
+            gender.Append(char.ToUpper(trimVersionOfGender[0]))
+                  .Append(trimVersionOfGender.Substring(1));
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Nick Name of Cat: {NickName}");
+            Console.WriteLine($"Age of Cat : {Age} years old");
+            Console.WriteLine($"Meal Quantitiy of Cat: {MealQuantity} gr");
+            Console.WriteLine($"Price of Cat : {Price} $");
+            Console.WriteLine($"Gender of Cat : {gender}");
+            Console.WriteLine($"Energy of Cat : {Energy}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Meal Quantity of Cat for Today: {_mealQuantityOfCatToday} gr");
+            Console.WriteLine("===================================");
+            Console.WriteLine();
+            Console.ResetColor();
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Cat_House
+{
+    class Program
+    {
+        static void Run()
+        {
+            try
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(); 
+                Console.WriteLine();
+                Console.Write(new string('║', Console.WindowWidth));
+                Console.WriteLine();
+                Console.Write(new string(' ', (Console.WindowWidth - "CHOX GOZAL PETI SHOP".Length) / 2));
+                Console.WriteLine("CHOX GOZAL PETI SHOP");
+                Console.Write(new string('║', Console.WindowWidth));
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(); 
+                Console.WriteLine();
+                Console.Write(new string('║', Console.WindowWidth));
+                Console.WriteLine();
+                Console.Write(new string(' ', (Console.WindowWidth - "CHOX GOZAL PETI SHOP 222".Length) / 2));
+                Console.WriteLine("CHOX GOZAL PETI SHOP 222");
+                Console.Write(new string('║', Console.WindowWidth));
+                Console.ResetColor();
+
+                Cat cat1 = new Cat("Kanvass", 6, 400, 10, "male");
+                Cat cat2 = new Cat("Toplan", 2, 250, 20, "male");
+                Cat cat4 = new Cat("BOB", 6, 400, 10, "male");
+                Cat cat5 = new Cat("TUMBO", 2, 250, 20, "male");
+                CatHouse catHouse = new CatHouse("CHOX GOZAL PETI SHOP");
+                catHouse.AddCat(cat1);
+                catHouse.AddCat(cat2);
+                CatHouse catHouse2 = new CatHouse("CHOX GOZAL PETI SHOP 222");
+                catHouse2.AddCat(cat4);
+                catHouse2.AddCat(cat5);
+                Cat cat3 = new Cat("Haykanush", 4, 500, 15, "Female");
+                catHouse.AddCat(cat3);
+                PetShop petShop = new PetShop();
+                petShop.AddCatHouse(catHouse);
+                petShop.AddCatHouse(catHouse2);
+                petShop.Show();
+                Console.WriteLine();
+                Console.WriteLine($"All Cats Price in Cat Houses: {petShop.GetCatsPriceInTheCatHouses()}");
+
+                //for (int i = 0; i < 12; i++) // Narmalni ishdeyir
+                //{
+                //cat1.Play();
+                //cat1.Eat();
+                //}
+
+                //Cat removedCat = catHouse.RemoveByNickName(cat3.NickName); // Narmalni ishdeyir narahat olmayun
+                //removedCat.Show();
+                //Console.WriteLine();
+                //Console.WriteLine();
+            }
+            catch (InvalidOperationException content)
+            {
+                Console.WriteLine(content);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Run();
+        }
+    }
+}
